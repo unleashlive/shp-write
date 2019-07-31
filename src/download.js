c3708be006f626fd313f88494310decbed063896
mordka <pawel@unleashlive.com>
2019-07-31 18:33:17 +0200

    feat: update file-saver

--- src/download.js
+++ src/download.js
@@ -2,5 +2,11 @@ var zip = require('./zip');
 var saveAs = require('file-saver').saveAs;
 
 module.exports = function(gj, options) {
-  zip(gj, options).then(function(blob) { saveAs(blob, options.file + '.zip'); });
+  zip(gj, options).then(function(blob) {
+    try {
+      var isFileSaverSupported = !!new Blob;
+    } catch (e) {
+      console.error('Saving files is not supported', e);
+    }
+    saveAs(blob, options.file + '.zip'); });
 };
